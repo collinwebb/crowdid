2cb0c6ff4df2f515ed44ab1cf8408d61fe92e7fd
Collin Webb <collindwebb@gmail.com>
2015-08-06 14:15:20 -0700

    merges nav bars

---
+++ app/overrides/edit_nav_bars.rb
@@ -0,0 +1,20 @@
+Deface::Override.new(
+  :virtual_path => "spree/shared/_nav_bar",
+  :insert_bottom => "ul#nav-bar",
+  :text => %Q(
+    <li id="link-to-cart" data-hook>
+      <noscript>
+        <%= link_to Spree.t(:cart), '/cart' %>
+      </noscript>
+      &nbsp;
+    </li>
+    <script>Spree.fetch_cart()</script>
+  ),
+  :name => "show_nav_bar"
+)
+
+Deface::Override.new(
+  :virtual_path => "spree/shared/_main_nav_bar",
+  :remove => "nav",
+  :name => "hide_main_nav_bar"
+)
