a2d340afbef512cd5b472afa8fb4d66f8efe3dd1
Collin Webb <collindwebb@gmail.com>
2015-08-15 17:32:50 -0700

    bundle updates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804214260) do
+ActiveRecord::Schema.define(version: 20150806000821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -963,6 +963,7 @@ ActiveRecord::Schema.define(version: 20150804214260) do
     t.integer  "tax_category_id"
     t.datetime "updated_at"
     t.integer  "stock_items_count",                          default: 0,     null: false
+    t.decimal  "sale_price",        precision: 8,  scale: 2
   end
 
   add_index "spree_variants", ["deleted_at"], name: "index_spree_variants_on_deleted_at", using: :btree
